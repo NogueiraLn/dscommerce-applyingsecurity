1211383d733bb2287ae03409d70a2c4db45b6845
NogueiraLn <lucas.nogueiramartins@gmail.com>
2025-08-10 19:37:15 -0300

    OrderDTO, get order by id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ClientMinDTO.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.User;
+
+public class ClientMinDTO {
+
+	private Long id;
+	private String name;
+
+	public ClientMinDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public ClientMinDTO(User entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
