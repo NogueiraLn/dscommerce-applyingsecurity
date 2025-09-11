b3d8ae5c8a9f18f8f9f06d75efa7eab4d0f01e67
NogueiraLn <lucas.nogueiramartins@gmail.com>
2025-09-10 23:00:42 -0300

    Add: Validate self or admin

--- src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
@@ -25,8 +25,8 @@ public class OrderController {
 
     @Autowired
     private OrderService service;
-
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
     @GetMapping(value = "/{id}")
     public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
         OrderDTO dto = service.findById(id);
