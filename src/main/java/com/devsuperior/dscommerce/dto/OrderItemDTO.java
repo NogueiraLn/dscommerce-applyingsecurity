f1131a03239abaf21d320adc676b045497496884
NogueiraLn <lucas.nogueiramartins@gmail.com>
2025-09-10 22:48:41 -0300

    Add: ImgUrl in Order

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -8,12 +8,15 @@ public class OrderItemDTO {
 	private String name;
 	private Double price;
 	private Integer quantity;
+	private String imgUrl;
+	
 
-	public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+	public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
 		this.productId = productId;
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
+		this.imgUrl = imgUrl;
 	}
 
 	public OrderItemDTO(OrderItem entity) {
@@ -21,6 +24,7 @@ public class OrderItemDTO {
 		this.name = entity.getProduct().getName();
 		this.price = entity.getPrice();
 		this.quantity = entity.getQuantity();
+		this.imgUrl = entity.getProduct().getImgUrl();
 
 	}
 
@@ -60,4 +64,12 @@ public class OrderItemDTO {
 		return price * quantity;
 	}
 
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	
 }
